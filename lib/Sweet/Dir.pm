915810a8b1263fa50628064d93b3270a8cf3ff56
fibo <casati_gianluca@yahoo.it>
2013-06-26 16:16:04 +0100

    added path to Dir

--- lib/Sweet/Dir.pm
+++ lib/Sweet/Dir.pm
@@ -1,8 +1,25 @@
 package Sweet::Dir;
 use Moose;
+use namespace::autoclean;
 use MooseX::StrictConstructor;
 
 use Try::Tiny;
+
+use MooseX::Types::Path::Class;
+
+=head1 SYNOPSIS
+
+    my $dir = Sweet::Dir->new(path => '/path/to/dir');
+
+=cut
+
+has 'path' => (
+builder=>'_build_path',
+coerce=>1,
+is=>'ro',
+isa=>'Path::Class::Dir',
+required=>1,
+);
 
 
 __PACKAGE__->meta->make_immutable;
