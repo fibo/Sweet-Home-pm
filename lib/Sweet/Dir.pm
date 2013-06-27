228c703e40f71faf1d1a58b48418d31581555931
fibo <casati_gianluca@yahoo.it>
2013-06-27 18:28:23 +0100

    added erase and create

--- lib/Sweet/Dir.pm
+++ lib/Sweet/Dir.pm
@@ -6,10 +6,12 @@ use MooseX::StrictConstructor;
 use Try::Tiny;
 
 use MooseX::Types::Path::Class;
+use File::Path qw(make_path remove_tree);
 
 =head1 SYNOPSIS
 
     my $dir = Sweet::Dir->new(path => '/path/to/dir');
+    $dir->create;
 
 =cut
 
@@ -21,6 +23,31 @@ isa=>'Path::Class::Dir',
 required=>1,
 );
 
+sub is_a_directory { 
+return -d shift->path;
+}
+
+sub create {
+my $self = shift;
+my $path = $self->path;
+my $make_path_error;
+
+return $self if $self->is_a_directory;
+
+make_path($path, {error => \$make_path_error});
+
+#TODO lancia eccezione, scrivi classe eccezioni, test e aggiorna synopsis
+}
+
+sub erase {
+my $self = shift;
+my $path = $self->path;
+my $remove_path_error;
+
+remove_path($path, {error => \$remove_path_error});
+
+#TODO lancia eccezione, scrivi classe eccezioni, test e aggiorna synopsis
+}
 
 __PACKAGE__->meta->make_immutable;
 
---
+++ t/dir.t
@@ -0,0 +1,9 @@
+use strict;
+use warnings;
+use Test::More;
+use Test::use::ok;
+
+use ok 'Sweet::Dir';
+
+done_testing;
+
