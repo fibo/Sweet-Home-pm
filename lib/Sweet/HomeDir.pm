7ed4a8405c094a2d34acc597e9711eab18f01a89
fibo <casati_gianluca@yahoo.it>
2014-06-23 15:01:10 +0200

    default to $HOME

--- lib/Sweet/HomeDir.pm
+++ lib/Sweet/HomeDir.pm
@@ -3,10 +3,13 @@ package Sweet::HomeDir;
 use Moose;
 use MooseX::StrictConstructor;
 
+use File::HomeDir;
+
 extends 'Sweet::Dir';
 
-use Try::Tiny;
-
+sub _build_path {
+  return File::HomeDir->my_home;
+  }
 
 __PACKAGE__->meta->make_immutable;
 
