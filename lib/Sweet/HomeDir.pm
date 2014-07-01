2f87dfe85b282949de365ac903f394ea5c182dcd
fibo <casati_gianluca@yahoo.it>
2014-07-01 16:30:19 +0200

    fixed tests

--- lib/Sweet/HomeDir.pm
+++ lib/Sweet/HomeDir.pm
@@ -1,15 +1,11 @@
 package Sweet::HomeDir;
-
 use Moose;
-use MooseX::StrictConstructor;
 
 use File::HomeDir;
 
 extends 'Sweet::Dir';
 
-sub _build_path {
-    return File::HomeDir->my_home;
-}
+sub _build_path { File::HomeDir->my_home }
 
 __PACKAGE__->meta->make_immutable;
 
